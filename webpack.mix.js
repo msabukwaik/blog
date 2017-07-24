bbd15a3ce41d648bbd9d32cfd15de27ebe842c93
msabukwaik <msabukwaik@gmail.com>
2017-07-24 20:08:36 +0300

    frontend

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,5 +13,5 @@ const { mix } = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
-mix.less('resources/assets/less/app.less', 'public/css');
+   .sass('resources/assets/sass/app.scss', 'public/css')
+    .scripts(['public/js/app.js'], 'public/js/js.min');
